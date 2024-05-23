d8b2cc590325366849628a6d90e7f6f01259936c
Navneet <hunnyhunnyhunny@gmail.com>
2024-05-22 21:07:52 -0400

    Dark theme toggle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ const Hero = () => {
       <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
           <h1 className="text-7xl font-bold tracking-wider">I am Navneet</h1>
-          <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
+          <p className="mt-4 text-3xl text-secondary capitalize tracking-wide">
             Full Stack Developer
           </p>
-          <p className="mt-2 text-lg text-slate-700 capitalize tracking-wide">
+          <p className="mt-2 text-lg text-secondary capitalize tracking-wide">
             turning ideas into interactive reality
           </p>
           <div className="flex gap-x-4 mt-4">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import { links } from "../data";
-
+import { themeChange } from "theme-change";
+import { useEffect } from "react";
 const Navbar = () => {
+  useEffect(() => {
+    themeChange(false);
+  }, []);
+
   return (
     <nav className="">
       <div className="align-element py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8">
         <h2 className="text-3xl font-bold">
-          Navneet <span className="text-emerald-600">Kahlon</span>
+          Navneet <span className="text-accent">Kahlon</span>
         </h2>
         <div className="flex gap-x-3">
           {links.map((link) => {
@@ -21,6 +26,41 @@ const Navbar = () => {
             );
           })}
         </div>
+
+        <label className="flex cursor-pointer gap-2 absolute right-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <circle cx="12" cy="12" r="5" />
+            <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
+          <input
+            type="checkbox"
+            value="business"
+            className="toggle theme-controller"
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
       </div>
     </nav>
   );
